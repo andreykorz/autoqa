231326b61a775df0a4f5d7e6d49c22623ed8a2b6
andreykorz <andreykorzhov@coherentsolutions.com>
2022-06-22 17:58:39 +0300

    Glory 2

--- Program.cs
+++ Program.cs
@@ -4,35 +4,17 @@ namespace MyFirstProject
 {
     class Program
     {
-        // kakouto koment
-        static void Main(string[] args)
+        static void GetReverseValue(int[] array)
         {
-            int[] array = new int[5];
-            array[0] = 2;
-            array[1] = array[0] * array[0];
-            array[2] = array[0] + array[1];
-            
-            Console.WriteLine("Result = {0}", array[0]);
-            Console.WriteLine("Result = {0}", array[1]);
-            Console.WriteLine("Result = {0}", array[2]);
+            int[] ReverseValue = array[0];
 
-            long[] square = new long[5];
-            square[0] = 6;
-            square[1] = square[0] * square[0];
-            square[2] = square[0] + square[1];
+            for (int index = array.Length - 1; index >= 0; index--)
+                
+            {
+                ReverseValue = array[index];                                   
+            }
+            // koment 2
 
-            Console.WriteLine("Result = {0}", square[0]);
-            Console.WriteLine("Result = {0}", square[1]);
-            Console.WriteLine("Result = {0}", square[2]);
-
-            float[] cube = new float[5];
-            cube[0] = array[0] + square[0];
-            cube[1] = array[1] + square[1];
-            cube[2] = array[2] + square[2];
-
-            Console.WriteLine("Result = {0}", cube[0]);
-            Console.WriteLine("Result = {0}", cube[1]);
-            Console.WriteLine("Result = {0}", cube[2]);
-        }
+        }Console.WriteLine(ReverseValue);
     }
 }
